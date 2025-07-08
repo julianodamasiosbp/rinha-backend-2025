85470d70c6b107c4b53e893c09eee53e8948c5b2
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-07-08 20:19:59 -0300

    add base project

---
+++ src/test/java/br/com/acme/rinha_backend/RinhaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acme.rinha_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
